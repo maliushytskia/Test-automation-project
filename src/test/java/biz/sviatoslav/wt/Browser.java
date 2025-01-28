bcd6b27d78ace1128a7bccf1902a2aa151605930
a.maliushytski <a.maliushytski@gmail.com>
2025-01-28 09:38:42 +0100

    MOD: Browser change(options)

--- src/test/java/biz/sviatoslav/wt/Browser.java
+++ src/test/java/biz/sviatoslav/wt/Browser.java
@@ -20,8 +20,8 @@ public class Browser {
                     WebDriverManager.chromedriver().setup();
                     options = new ChromeOptions();
                     options.addArguments("--no-sandbox");
-                    options.addArguments("--disable-dev-shm-usage");
-                    options.addArguments("--user-data-dir=/tmp/chrome_" + System.currentTimeMillis());
+                    options.addArguments("--disable-extensions");
+                    options.addArguments("--incognito");
                     driver = new ChromeDriver();
                     break;
                 case "firefox":
