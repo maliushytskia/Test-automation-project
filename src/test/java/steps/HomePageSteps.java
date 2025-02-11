b84f7cc1a62775897dc4c04cbdd0568d6f9730d3
a.maliushytski <a.maliushytski@gmail.com>
2025-02-11 15:39:05 +0100

    ADD: adding allure-cucumber5-jvm

--- src/test/java/steps/HomePageSteps.java
+++ src/test/java/steps/HomePageSteps.java
@@ -2,6 +2,7 @@ package steps;
 
 import core.Logger;
 import io.cucumber.java.en.Given;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import pages.HomePage;
 
@@ -9,12 +10,14 @@ public class HomePageSteps {
     private HomePage homePage;
     WebDriver driver;
 
+    @Step("Home Page is opened")
     @Given("Home Page is opened")
     public void homePageOpened() {
         Logger.getInstance().info("Home page opening");
         homePage = new HomePage();
     }
 
+    @Step("user opens Shop by Category panel")
     @Given("user opens Shop by Category panel")
     public void openShopByCategoryPanel() {
         Logger.getInstance().info("Shop by Category panel opening");
