30df445e0a8b7599769335ca7bc70fdc358436c1
a.maliushytski <a.maliushytski@gmail.com>
2025-02-12 14:42:04 +0100

    ADD: adding allure-cucumber5-jvm

--- src/test/java/steps/runners/TestRunner.java
+++ src/test/java/steps/runners/TestRunner.java
@@ -1,6 +1,7 @@
 package steps.runners;
 
 import io.cucumber.junit.CucumberOptions;
+import org.junit.Test;
 import org.junit.platform.suite.api.*;
 
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
@@ -16,17 +17,15 @@ import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 //        },
 //        monochrome = true
 //)
-
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @CucumberOptions(
         plugin = {
                 "pretty",
                 "json:target/cucumber-report/cucumber.json",
-                "io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
         },
         dryRun = false, monochrome = true
 )
