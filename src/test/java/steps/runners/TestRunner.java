37de133d30083009f81b62cca8eff9073037267d
a.maliushytski <a.maliushytski@gmail.com>
2025-03-11 20:01:37 +0100

    ADD: added needed settings for Allure report to make Cucumber steps visible

--- src/test/java/steps/runners/TestRunner.java
+++ src/test/java/steps/runners/TestRunner.java
@@ -13,7 +13,7 @@ import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
         plugin = {
                 "pretty",
                 "json:target/cucumber-report/cucumber.json",
-                "io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
         },
         dryRun = false,
         monochrome = true,
